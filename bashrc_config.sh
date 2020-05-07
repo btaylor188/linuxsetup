1ee94f1e50df4fe0dc72a69f6b5b6b3e92fcc8db
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-05-07 16:26:22 -0600

    Update bashrc_config.sh

--- bashrc_config.sh
+++ bashrc_config.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 echo 'export XAUTHORITY=~/.Xauthority' >> ~/.bashrc
-echo 'alias ll='ls -lha --color=auto'' >> ~/.bashrc
+echo "alias ll='ls -lha --color=auto'" >> ~/.bashrc
 echo 'alias cls=clear' >> ~/.bashrc
 echo 'export PS1="\[\033[38;5;10m\]\u\[$(tput sgr0)\]@\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]:\[$(tput sgr0)\]\[\033[38;5;14m\]\w\[$(tput sgr0)\]\\$\[$(tput sgr0)\]"' >> ~/.bashrc
