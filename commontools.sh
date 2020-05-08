fe7f290ec060e8975a8ba57afb3f56ca8f9f24ff
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-05-08 10:50:18 -0600

    Update commontools.sh

--- commontools.sh
+++ commontools.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 yum --enablerepo=extras install epel-release; -y
+yum update -y
 yum install htop screen iftop wget zip  unzip ntfs-3g nano mc net-tools curl nfs-common tar net-tools   -y
+apt update -y
 apt install htop screen iftop wget zip  unzip ntfs-3g nano mc net-tools curl nfs-common tar net-tools   -y
+
