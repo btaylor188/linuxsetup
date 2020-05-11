5a5b82487a7053be5a805fc45d0a48dc280bb487
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2020-05-11 17:48:09 -0600

    Update and rename x11-centos.sh to x11.sh

--- x11.sh
+++ x11.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
 sudo yum install  xorg-x11-server-Xorg xorg-x11-xauth xorg-x11-apps gedit xfe -y
+sudo apt update -y
+sudo apt install xauth xorg openbox dconf-editor dbus-x11 gedit xfe -y
