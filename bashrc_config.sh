d677ea04cc673c9fa0ec552c31781111b04c63f2
btaylor188 <brandon@deepthought.int.interwebnet.net>
2020-12-14 18:05:25 -0700

    added docker rate limit script

--- bashrc_config.sh
+++ bashrc_config.sh
@@ -2,5 +2,6 @@
 echo 'export XAUTHORITY=~/.Xauthority' >> ~/.bashrc
 echo "alias ll='ls -lha --color=auto'" >> ~/.bashrc
 echo 'alias cls=clear' >> ~/.bashrc
+echo "alias du1='du -h -d 1'" >> ~/.bashrc
 echo 'export PS1="\[\e[32m\]\u\[\e[m\]@\[\e[31m\]\h\[\e[m\]:\[\e[33m\]\A\[\e[m\]:\[\e[36m\]\w\[\e[m\]\[\e[36m\]\\$\[\e[m\] "' >> ~/.bashrc
 bash
