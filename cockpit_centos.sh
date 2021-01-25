d422ccb951d22e69e31bb29ad4be2d92bd5937fc
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2021-01-25 12:15:55 -0700

    Update cockpit_centos.sh

--- cockpit_centos.sh
+++ cockpit_centos.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-sudo yum install cockpit
+sudo yum install cockpit cockpit-machines cockpit-docker qemu-kvm libvirt virt-install virt-viewer
 sudo systemctl enable --now cockpit.socket
+sudo systemctl enable libvirtd --nowsudo 
 sudo firewall-cmd --permanent --zone=public --add-service=cockpit
 sudo firewall-cmd --reload
