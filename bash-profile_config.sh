19bca6b4f837adcb83776dad3697d3e751715b3e
brandon <brandon@brandon-taylor.net>
2022-09-08 01:25:58 +0000

    reorder

--- bash-profile_config.sh
+++ bash-profile_config.sh
@@ -4,5 +4,5 @@ echo "alias ll='ls -lha --color=auto'" >> ~/.bashrc
 echo 'alias cls=clear' >> ~/.bashrc
 echo "alias du1='du -h -d 1'" >> ~/.bashrc
 echo 'export PS1="\[\e[32m\]\u\[\e[m\]@\[\e[31m\]\h\[\e[m\]:\[\e[33m\]\A\[\e[m\]:\[\e[36m\]\w\[\e[m\]\[\e[36m\]\\$\[\e[m\] "' >> ~/.bashrc
-bash
 echo 'git config --global credential.helper "cache --timeout=86400"' >> ~/.profile
+bash
