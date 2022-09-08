37ed44ce8f17c51444f7fc29f549570137f687e8
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2022-09-07 20:55:51 -0600

    Update commontools.sh

--- commontools.sh
+++ commontools.sh
@@ -6,6 +6,6 @@ sudo yum update -y
 sudo apt upgrade -y
 sudo yum install htop screen iftop wget zip  unzip ntfs-3g nano mc net-tools curl nfs-common tar net-tools links   -y
 sudo apt update -y
-sudo apt install htop screen iftop wget zip  unzip ntfs-3g nano mc net-tools curl nfs-common tar net-tools links   -y
+sudo apt install htop screen iftop wget zip  unzip ntfs-3g nano mc net-tools curl nfs-common tar net-tools links bsdmainutils -y
 sudo apt upgrade -y
 git config --global credential.helper "cache --timeout=86400"
