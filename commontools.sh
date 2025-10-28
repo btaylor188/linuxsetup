ec95837a911636de9aadff76435f73084f50ab45
btaylor188 <64984258+btaylor188@users.noreply.github.com>
2025-10-28 11:33:20 -0600

    Switch from yum to dnf for package management

--- commontools.sh
+++ commontools.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 echo "############   Installing Updates and Common Tools###############"
 echo "############       This will take some time...    ###############"
-sudo yum --enablerepo=extras install epel-release; -y > commontools.log 2>&1
-sudo yum update -y
+sudo dnf --enablerepo=extras install epel-release; -y > commontools.log 2>&1
+sudo dnf update -y
 sudo apt upgrade -y
-sudo yum install htop screen iftop wget zip unzip ntfs-3g nano mc net-tools curl nfs-utils tar net-tools links   -y
+sudo dnf install htop screen iftop wget zip unzip ntfs-3g nano mc net-tools curl nfs-utils tar net-tools links   -y
 sudo apt update -y
 sudo apt install htop screen iftop wget zip  unzip ntfs-3g nano mc net-tools curl nfs-common tar net-tools links bsdmainutils -y
 sudo apt upgrade -y
